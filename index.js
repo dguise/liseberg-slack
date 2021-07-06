ab9ea4aee417dfadba66a78be1636e1a4461f2d2
Maakep <schon.fredrik@gmail.com>
2021-07-06 18:53:42 +0200

    Add hourly capacity check

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const path = process.env.slack_path;
 appStart();
 
 setInterval(getCapacity, 5 * 60 * 1000);
+setInterval(displayCapacity, 60 * 60 * 1000);
+displayCapacity();
 getCapacity();
 
 async function getCapacity() {
@@ -25,6 +27,24 @@ async function getCapacity() {
       alertSlackAboutTime(`:crescent_moon: ${day} - (${d.eveningCapacity})`);
     }
   });
+}
+
+async function displayCapacity() {
+  console.log("Hourly capacity check...");
+  const res = await fetch(
+    "https://www.liseberg.se/sv/api/capacity?from=2021-07-21&to=2021-07-23"
+  );
+  const json = await res.json();
+  let message = ":exclamation: Hourly capacity check! \r\n ";
+  json.dates.forEach((d) => {
+    const date = new Date(d.date);
+    const day = `${date.getDate()}/${date.getMonth() + 1}`;
+
+    message += `:sunny: ${day} (${d.capacity}) \r\n `;
+    message += `:crescent_moon: ${day} (${d.eveningCapacity}) \r\n `;
+    message += `\r\n `;
+  });
+  slack(message);
 }
 
 function alertSlackAboutTime(day) {
